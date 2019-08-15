bef71dfd1ea891a2b7f686e80b1f4deba7e9debe
fleonis <fleonis@noemail.net>
2019-08-15 21:09:16 +0000

    section in file
    FossilOrigin-Name: 222c8ab768e66b9c701e01d0da3b140c9851a7048b552435f83f3c81e7d39bfe

--- default.nix
+++ default.nix
@@ -7,12 +7,23 @@ let
   callPackage = stdenv.lib.callPackageWith self;
   self = rec {
     inherit stdenv enable_clcov pkgs callPackage;
+
+    # compilers utils
+    cmake = pkgs.cmake;
+    ninja = pkgs.ninja; 
+    clang = pkgs.llvmPackages_8.clang;
+    llvm = pkgs.llvm_8;
+
+    # own projects
     modegen = callPackage ./tools/modegen { };
     cppjinja = callPackage ./libs/cppjinja {};
+
+    # libraries and tools
     helpers = callPackage ./helpers.nix {};
     boost = pkgs.boost16x;
-
     py_jinja = pkgs.python3Packages.jinja2;
+
+    # patched libraries
     python3 = pkgs.python3.overrideAttrs( old : {
         postPatch = old.postPatch + ''
         sed -i '55d' Modules/_decimal/libmpdec/context.c
@@ -24,11 +35,6 @@ let
             doCheck = true;
             cmakeFlags = old.cmakeFlags ++ ["-DPYBIND11_TEST=OFF"];
         }) ;
-
-    cmake = pkgs.cmake;
-    ninja = pkgs.ninja; 
-    clang = pkgs.llvmPackages_8.clang;
-    llvm = pkgs.llvm_8;
   };
 in with self; rec {
   inherit modegen cppjinja;
