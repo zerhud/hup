95a6519ddfbb2c6dcc6aaa8ee0f4ac6a86d08331
fleonis <fleonis@noemail.net>
2019-09-04 20:35:43 +0000

    clang8 cannot compile cppjinja..
    FossilOrigin-Name: f07ab52e51937e6d5c07a6e1172cc733b411e84831f288d7c35b34c42fe17b45

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
     system ? builtins.currentSystem
   , pkgs ? import<nixos-unstable> { inherit system; }
   , enable_clcov ? true
-  , stdenv ? pkgs.clangStdenv
+  , stdenv ? pkgs.gcc9Stdenv
 }:
 import ./default.nix {
   inherit pkgs stdenv enable_clcov;
