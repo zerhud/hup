1ef1ae2b56ac6fe12b6975a206a8af341f7314ae
fleonis <fleonis@noemail.net>
2019-12-15 23:11:23 +0000

    add nlohman json and turtle to cppjinja
    FossilOrigin-Name: e85351fca439a53428179e5813d1f69609d768883aa305fce8a66f4de736c86a

--- libs/cppjinja/default.nix
+++ libs/cppjinja/default.nix
@@ -3,6 +3,7 @@
   , boost_shared
   , cmake
   , ninja
+  , helpers
 }:
 stdenv.mkDerivation {
   name = "cppjinja-alfa";
@@ -12,8 +13,10 @@ stdenv.mkDerivation {
     site = "https://cpphttpx.org/repos/cppjinja";
   };
 
+  nlohman_json_header = helpers.nlohman_json_header;
+
   nativeBuildInputs = [ cmake ninja ];
-  buildInputs = [ boost_shared ];
+  buildInputs = [ boost_shared helpers.turtle ];
 
   src = builtins.fetchurl {
     url = "http://cpphttpx.org/repos/cppjinja/tarball/e8f0c81bfc/cppjinja_-e8f0c81bfc.tar.gz";
